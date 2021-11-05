07170f4ed05a140ded42140791fef9dc0b209502
Khanh Minh Dao <minhdao933@gmail.com>
2021-11-05 19:50:25 +0700

    commit admin

---
+++ DoAnCN/Project/admin/layoutAdmin/footer.php
@@ -0,0 +1,9 @@
+<script src="../admin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../admin/plugins/chart.js/Chart.min.js"></script>
+<script src="../admin/dist/js/adminlte.min.js"></script>
+</body>
+</html>
+
